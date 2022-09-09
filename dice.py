bba73da21e528bb92371209eea9c380206f2d43f
Dan Cristea <35790451+DannyCristea@users.noreply.github.com>
2022-09-09 15:56:11 -0700

    Add files via upload

---
+++ dice.py
@@ -0,0 +1,59 @@
+from random import randint
+
+class Die:
+    def __init__(self):
+        pass
+        
+    def dice():
+        s = randint(1,6)
+        if s == 1:
+            print(" __________ ")
+            print("|          |")
+            print("|          |")
+            print("|    ()    |")
+            print("|          |")
+            print("|__________|")
+            print("    ONE     ")
+        elif s == 2:
+            print(" __________ ")
+            print("|          |")
+            print("|  ()      |")
+            print("|          |")
+            print("|      ()  |")
+            print("|__________|")
+            print("    TWO     ")
+        elif s == 3:
+            print(" __________ ")
+            print("|          |")
+            print("|  ()      |")
+            print("|    ()    |")
+            print("|      ()  |")
+            print("|__________|")
+            print("   THREE    ")
+        elif s == 4:
+            print(" __________ ")
+            print("|          |")
+            print("|  ()  ()  |")
+            print("|          |")
+            print("|  ()  ()  |")
+            print("|__________|")
+            print("    FOUR    ")
+        elif s == 5:
+            print(" __________ ")
+            print("|          |")
+            print("|  ()  ()  |")
+            print("|    ()    |")
+            print("|  ()  ()  |")
+            print("|__________|")
+            print("    FIVE    ")
+        elif s == 6:
+            print(" __________ ")
+            print("|          |")
+            print("|  ()  ()  |")
+            print("|  ()  ()  |")
+            print("|  ()  ()  |")
+            print("|__________|")
+            print("    SIX     ")
+
+
+    dice()
